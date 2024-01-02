7830fecca00c0efb235037b7cf0b6122351ad1d1
BurnyLlama <burnyllama@protonmail.com>
2024-01-02 19:25:36 +0100

    Moved search to Item model.

--- routes/api.php
+++ routes/api.php
@@ -62,61 +62,7 @@ Route::get('/1.0.0/item/search', function (Request $request) {
     $order_by = $request->query('sort', 'name');
     $reverse = $request->query('reverse', 'false');
 
-    // Filter for only valid types.
-    $types_as_array = is_array($types) ? $types : preg_split('/,/', $types);  // Make sure `types` isn't an array already, before splitting it into one.
-    $valid_types = array_filter($types_as_array, function ($type) {
-        return in_array($type, HusmusenItem::$valid_types);
-    });
-    $types_sql = "('" . join("','", sizeof($valid_types) != 0 ? $valid_types : HusmusenItem::$valid_types) . "')";
-
-    // Make sure `keyword_mode` is not an array.
-    $keyword_mode_as_string = is_array($keyword_mode) ? end($keyword_mode) : $keyword_mode;
-    // Make sure it is 'AND' or 'OR'.
-    $keyword_mode_sane = $keyword_mode_as_string == 'AND' ? 'AND' : 'OR';
-
-    $keywords_as_array = is_array($types) ? $keywords : preg_split('/,/', $keywords);  // Make sure `keyword` isn't an array already, before splitting it into one.
-    // TODO: Validate the keywords.
-    $valid_keywords = array_filter($keywords_as_array, function ($keyword) {
-        return true;
-    });
-
-    // Create keyword SQL; slightly magical. :|
-    $keyword_search_sql = $keyword_mode === 'AND'
-        // If in "AND-mode", use this magic RegEx created here:
-        // This also requires the keywords to be sorted alphabetically.
-        ? ($valid_keywords ? "AND keywords RLIKE '(?-i)(?<=,|^)(" . join('(.*,|)', $valid_keywords) . ')(?=,|$)\'' : '')
-        // Otherwise, use "OR-mode" with this magic RegEx:
-        : ($valid_keywords ? "AND keywords RLIKE '(?-i)(?<=,|^)(" . join('|', $valid_keywords) . ')(?=,|$)\'' : '');
-
-    $VALID_SORT_FIELDS = array('name', 'relevance', 'lastUpdated', 'addedAt', 'itemID');
-    // Make sure `order_by` is not an array.
-    $order_by_as_string = is_array($order_by) ? end($order_by) : $order_by;
-    // Make sure it is 'AND' or 'OR'.
-    $order_by_sane = in_array($order_by_as_string, $VALID_SORT_FIELDS) ? $order_by_as_string : 'name';
-
-    /**
-     * This formula figures out if the results should be reversed or not.
-     * The complexity is needed because the 'relevance' search option works
-     * the other way around compared to all other sorting otions.
-     */
-    $should_reverse_order = $order_by == 'relevance'
-        ? ($reverse == '1' || $reverse == 'on' || $reverse == 'true' ? 'ASC' : 'DESC')
-        : ($reverse == '1' || $reverse == 'on' || $reverse == 'true' ? 'DESC' : 'ASC');
-
-    // FIXME: Find a way to make sure this is safe and no SQL-incations...
-    $sanitized_freetext = $freetext;
-
-    $magic_relevance_sql = "((MATCH(name) AGAINST('$sanitized_freetext' IN BOOLEAN MODE) + 1) * (MATCH(description) AGAINST('$sanitized_freetext' IN BOOLEAN MODE) + 1) - 1) / 3";
-    $magic_relevance_search_sql = $freetext != null ? "AND (MATCH(name) AGAINST('$sanitized_freetext' IN BOOLEAN MODE) OR MATCH(description) AGAINST('$sanitized_freetext' IN BOOLEAN MODE))" : '';
-
-    return DB::select("
-        SELECT *, ($magic_relevance_sql) AS relevance
-            FROM husmusen_items
-            WHERE type IN $types_sql
-            $keyword_search_sql
-            $magic_relevance_search_sql
-            ORDER BY $order_by_sane $should_reverse_order
-        ");
+    return HusmusenItem::search_v1_0_0($types, $freetext, $keywords, $keyword_mode, $order_by, $reverse);
 });
 
 Route::get('/1.0.0/item/info/{id}', function (string $id) {
