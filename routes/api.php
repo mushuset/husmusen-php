3d416160e588cf56444e8cca6514951d0d33c510
furo <furo@posteo.net>
2024-08-03 19:56:43 +0200

    Ability to mark item as expired

--- routes/api.php
+++ routes/api.php
@@ -69,6 +69,25 @@ Route::get('/1.0.0/item/info/{id}', function (string $id) {
     return $item;
 });
 
+Route::post('/1.0.0/item/mark', function (Request $request) {
+    $item_id = $request->input('itemID');
+
+    $item = HusmusenItem::find($item_id);
+    if (!$item) {
+        return HusmusenError::SendError(404, 'ERR_ITEM_NOT_FOUND', 'It appears this item does not exist.');
+    }
+
+    $item->isExpired = true;
+    $item->expireReason = $request->input('reason');
+
+    $save_succeded = $item->save();
+    if (!$save_succeded) {
+        return HusmusenError::SendError(500, 'ERR_DATABASE_ERROR', 'Something went wrong while saving the item!');
+    }
+
+    return response()->json($item);
+});
+
 Route::get('/1.0.0/file/get/{id}', function (string $id) {
     // TODO: Return file data!
     return 'NOT IMPLEMENTED';
