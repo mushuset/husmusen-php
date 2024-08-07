b5c2c4084969d27f3bfdba418826ef0f4eaf0d03
BurnyLlama <burnyllama@protonmail.com>
2024-08-07 21:32:18 +0200

    Editing pages for DBInfo and File shold have full-width textarea.

--- resources/views/control_panel/edit_dbinfo.blade.php
+++ resources/views/control_panel/edit_dbinfo.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('head')
-    <script src="/resources/scripts/controlPanel.js" async defer type="module"></script>
+<script src="/resources/scripts/controlPanel.js" async defer type="module"></script>
 @endsection
 
 @section('body')
 @if(isset($err))
-        {{ $err }}
-    @else
-        <form action="/api/db_info" method="post" class="YAML">
-            <h1>Redigera databas information:</h1>
-            <textarea name="YAML" cols="120" rows="20" style="font-family: monospace">{{ $dbInfoAsYAML }}</textarea>
-            <input type="submit" value="Spara!">
-        </form>
-    @endif
+{{ $err }}
+@else
+<form action="/api/db_info" method="post" class="YAML">
+    <h1>Redigera databas information:</h1>
+    <textarea name="YAML" rows="20" class="full-width monospace">{{ $dbInfoAsYAML }}</textarea>
+    <input type="submit" value="Spara!">
+</form>
+@endif
 @endsection
--- resources/views/control_panel/edit_file.blade.php
+++ resources/views/control_panel/edit_file.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('head')
-    <script src="/resources/scripts/controlPanel.js" async defer type="module"></script>
+<script src="/resources/scripts/controlPanel.js" async defer type="module"></script>
 @endsection
 
 @section('body')
-    @if(isset($err))
-        {{ $err }}
-    @else
-        <form action="/api/1.0.0/file/edit" method="post" id="edit-file-form">
-            <h1>Redigera fil:</h1>
-            <input type="hidden" name="fileID" value="{{ $fileID }}">
-            <textarea name="newFileData" cols="120" rows="20" style="font-family: monospace">{{ $fileAsYAML}}</textarea>
-            <input type="submit" value="Redigera fil!">
-        </form>
-    @endif
+@if(isset($err))
+{{ $err }}
+@else
+<form action="/api/1.0.0/file/edit" method="post" id="edit-file-form">
+    <h1>Redigera fil:</h1>
+    <input type="hidden" name="fileID" value="{{ $fileID }}">
+    <textarea name="newFileData" rows="20" class="full-width monospace">{{ $fileAsYAML}}</textarea>
+    <input type="submit" value="Redigera fil!">
+</form>
+@endif
 @endsection
