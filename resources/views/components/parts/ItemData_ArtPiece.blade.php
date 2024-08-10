cdb22adc58affec0db4b0178271cbc558f1d9858
furo <furo@posteo.net>
2024-08-10 23:16:06 +0200

    Fix input macro

--- resources/views/components/parts/ItemData_ArtPiece.blade.php
+++ resources/views/components/parts/ItemData_ArtPiece.blade.php
@@ -6,4 +6,7 @@ weight: 8203
 year: 1956
 -->
 
-<x-item-data-input :type="text" :name="artist" :placeholder="artist" :hint=" " :value=" " />
+@include('components/item-data-input', 
+    ['type' => 'text', 'label' => 'Namn på artist', 'name' => 'artist',
+    'placeholder' => 'Bengt Svensson', 'hint' => 'Vad är det för namn på artisten?', 'value' => ''
+])
